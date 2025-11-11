fa0ced7cbf635d19e666b5280a52fe805c638755
FranklynSantos <franklynsantos451@gmail.com>
2025-11-11 20:15:05 -0300

    att

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿
+
+
+CREATE TABLE NEGOCIO (
+    idNegocio INT not null PRIMARY KEY identity(1,1),
+    valor DECIMAL(10,2),
+    tipo VARCHAR(50),
+    data DATE,
+    idUsuario INT,
+    idCliente INT,
+    idCarro INT,
+    FOREIGN KEY (idUsuario) REFERENCES USUARIO(idUsuario),
+    FOREIGN KEY (idCliente) REFERENCES CLIENTE(idCliente),
+    FOREIGN KEY (idCarro) REFERENCES Carro(idCarro)
+)
